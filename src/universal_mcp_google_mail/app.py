d07e5b310eaaa87e9e4068be527e1387a2d795a2
Ankit Ranjan <arstejas7@gmail.com>
2025-05-13 15:20:12 +0530

    minor fix

--- src/universal_mcp_google_mail/app.py
+++ src/universal_mcp_google_mail/app.py
@@ -3,7 +3,7 @@ from email.message import EmailMessage
 from typing import Any
 from loguru import logger
 
-from universal_mcp.applications.application import APIApplication
+from universal_mcp.applications import APIApplication
 from universal_mcp.exceptions import NotAuthorizedError
 from universal_mcp.integrations import Integration
 
